545f01668c4478d265bc6e5b0c53667a47e1730e
Rugved <T18D9M@gmail.com>
2021-06-24 17:02:37 +0530

    sign up page primary color 1 trail intent

--- app/src/main/java/com/Project/appointmentapp/MainActivity.java
+++ app/src/main/java/com/Project/appointmentapp/MainActivity.java
@@ -2,13 +2,21 @@ package com.Project.appointmentapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button trail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        trail=findViewById(R.id.signinbtn);
+        trail.setOnClickListener(v -> {
+            Intent registerScreen = new Intent(MainActivity.this, signup.class);
+            startActivity(registerScreen);
+        });
     }
 }
