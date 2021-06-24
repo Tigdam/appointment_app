5a2687089c5aed67b6a4dceb26e3915673a1e8a4
tigdam <T18D9M@gmail.com>
2021-06-24 17:37:17 +0530

    check calender

--- app/src/main/java/com/Project/appointmentapp/User_Profile.java
+++ app/src/main/java/com/Project/appointmentapp/User_Profile.java
@@ -2,23 +2,34 @@ package com.Project.appointmentapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+
+import java.util.Calendar;
 
 public class User_Profile extends AppCompatActivity {
 
     ImageView imageView;
     Spinner spinner;
+    EditText date_of_birth;
+    private int mYear, mMonth, mDay;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
         imageView = findViewById(R.id.profile_img);
+        date_of_birth = findViewById(R.id.dob);
 
         Spinner spinner = (Spinner) findViewById(R.id.gender_spinner);
+
 // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.gender_array, android.R.layout.simple_spinner_item);
@@ -26,6 +37,38 @@ public class User_Profile extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+
+        date_of_birth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (v == date_of_birth) {
+                    final Calendar c = Calendar.getInstance();
+                    mYear = c.get(Calendar.YEAR);
+                    mMonth = c.get(Calendar.MONTH);
+                    mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+                    DatePickerDialog datePickerDialog = new DatePickerDialog(User_Profile.this,
+                            new DatePickerDialog.OnDateSetListener() {
+
+                                @Override
+                                public void onDateSet(DatePicker view, int year,
+                                                      int monthOfYear, int dayOfMonth) {
+
+                                    date_of_birth.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                                }
+                            }, mYear, mMonth, mDay);
+                    datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis()-1000);
+                    datePickerDialog.show();
+                }
+
+
+
+
+            }
+        });
 
     }
 }
