492c01610b1e132b52986b579463b95d3eed3f95
tigdam <T18D9M@gmail.com>
2021-07-07 21:05:54 +0530

    admin

---
+++ app/src/main/java/com/Project/appointmentapp/UserHelperClass_DocEP.java
@@ -0,0 +1,119 @@
+package com.Project.appointmentapp;
+
+public class UserHelperClass_DocEP {
+    String Doc_Fname,Doc_Lname,Doc_Email,Doc_Mob,Doc_DOB,Doc_Qual,Doc_Exp,Doc_Spec,Doc_FTimme,Doc_TTime,Doc_About,Doc_Add;
+
+    public UserHelperClass_DocEP() {
+    }
+
+    public String getDoc_Fname() {
+        return Doc_Fname;
+    }
+
+    public void setDoc_Fname(String doc_Fname) {
+        Doc_Fname = doc_Fname;
+    }
+
+    public String getDoc_Lname() {
+        return Doc_Lname;
+    }
+
+    public void setDoc_Lname(String doc_Lname) {
+        Doc_Lname = doc_Lname;
+    }
+
+    public String getDoc_Email() {
+        return Doc_Email;
+    }
+
+    public void setDoc_Email(String doc_Email) {
+        Doc_Email = doc_Email;
+    }
+
+    public String getDoc_Mob() {
+        return Doc_Mob;
+    }
+
+    public void setDoc_Mob(String doc_Mob) {
+        Doc_Mob = doc_Mob;
+    }
+
+    public String getDoc_DOB() {
+        return Doc_DOB;
+    }
+
+    public void setDoc_DOB(String doc_DOB) {
+        Doc_DOB = doc_DOB;
+    }
+
+    public String getDoc_Qual() {
+        return Doc_Qual;
+    }
+
+    public void setDoc_Qual(String doc_Qual) {
+        Doc_Qual = doc_Qual;
+    }
+
+    public String getDoc_Exp() {
+        return Doc_Exp;
+    }
+
+    public void setDoc_Exp(String doc_Exp) {
+        Doc_Exp = doc_Exp;
+    }
+
+    public String getDoc_Spec() {
+        return Doc_Spec;
+    }
+
+    public void setDoc_Spec(String doc_Spec) {
+        Doc_Spec = doc_Spec;
+    }
+
+    public String getDoc_FTimme() {
+        return Doc_FTimme;
+    }
+
+    public void setDoc_FTimme(String doc_FTimme) {
+        Doc_FTimme = doc_FTimme;
+    }
+
+    public String getDoc_TTime() {
+        return Doc_TTime;
+    }
+
+    public void setDoc_TTime(String doc_TTime) {
+        Doc_TTime = doc_TTime;
+    }
+
+    public String getDoc_About() {
+        return Doc_About;
+    }
+
+    public void setDoc_About(String doc_About) {
+        Doc_About = doc_About;
+    }
+
+    public String getDoc_Add() {
+        return Doc_Add;
+    }
+
+    public void setDoc_Add(String doc_Add) {
+        Doc_Add = doc_Add;
+    }
+
+    public UserHelperClass_DocEP(String doc_Fname, String doc_Lname, String doc_Email, String doc_Mob, String doc_DOB, String doc_Qual, String doc_Exp, String doc_Spec, String doc_FTimme, String doc_TTime, String doc_About, String doc_Add) {
+        Doc_Fname = doc_Fname;
+        Doc_Lname = doc_Lname;
+        Doc_Email = doc_Email;
+        Doc_Mob = doc_Mob;
+        Doc_DOB = doc_DOB;
+        Doc_Qual = doc_Qual;
+        Doc_Exp = doc_Exp;
+        Doc_Spec = doc_Spec;
+        Doc_FTimme = doc_FTimme;
+        Doc_TTime = doc_TTime;
+        Doc_About = doc_About;
+        Doc_Add = doc_Add;
+    }
+}
