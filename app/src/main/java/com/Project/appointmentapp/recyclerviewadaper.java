4b565d22564f471761cc7c6bade24bc6466c18bf
Rugved <T18D9M@gmail.com>
2021-08-02 16:17:06 +0530

    cardview,admin pg half,

---
+++ app/src/main/java/com/Project/appointmentapp/recyclerviewadaper.java
@@ -0,0 +1,13 @@
+package com.Project.appointmentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class recyclerviewadaper extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
