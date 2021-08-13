b7e11adc584768eefc553a2e184d1e6a7af05111
Rugved <T18D9M@gmail.com>
2021-08-13 19:50:32 +0530

    doc dash,patnt list pg ,etc blank pg

---
+++ app/src/main/java/com/Project/appointmentapp/doc_detail_cv.java
@@ -0,0 +1,14 @@
+package com.Project.appointmentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class doc_detail_cv extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doc_detail_cv);
+    }
+}
---
+++ app/src/main/java/com/Project/appointmentapp/patient_list_page.java
@@ -0,0 +1,14 @@
+package com.Project.appointmentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class patient_list_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_list_page);
+    }
+}
